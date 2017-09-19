e2a2284c47dd7f4a4f255d1cee9f2099c40782ba
arjantijms <arjan.tijms@gmail.com>
2017-09-19 15:58:31 +0200

    Formatting

--- security/dynamic-rememberme/src/main/java/org/javaee8/security/dynamic/rememberme/util/HttpAuthenticationMechanismWrapper.java
+++ security/dynamic-rememberme/src/main/java/org/javaee8/security/dynamic/rememberme/util/HttpAuthenticationMechanismWrapper.java
@@ -8,33 +8,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HttpAuthenticationMechanismWrapper implements HttpAuthenticationMechanism {
-        
-        private HttpAuthenticationMechanism httpAuthenticationMechanism;
-        
-        public HttpAuthenticationMechanismWrapper() {
-        }
-        
-        public HttpAuthenticationMechanismWrapper(HttpAuthenticationMechanism httpAuthenticationMechanism) {
-            this.httpAuthenticationMechanism = httpAuthenticationMechanism;
-        }
-        
-        HttpAuthenticationMechanism getWrapped() {
-            return httpAuthenticationMechanism;
-        }
-        
-        @Override
-        public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthenticationException {
-            return getWrapped().validateRequest(request, response, httpMessageContext);
-        }
-        
-        @Override
-        public AuthenticationStatus secureResponse(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthenticationException {
-            return getWrapped().secureResponse(request, response, httpMessageContext);
-        }
-        
-        @Override
-        public void cleanSubject(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) {
-            getWrapped().cleanSubject(request, response, httpMessageContext);
-        }
-        
+
+    private HttpAuthenticationMechanism httpAuthenticationMechanism;
+
+    public HttpAuthenticationMechanismWrapper() {
     }
+
+    public HttpAuthenticationMechanismWrapper(HttpAuthenticationMechanism httpAuthenticationMechanism) {
+        this.httpAuthenticationMechanism = httpAuthenticationMechanism;
+    }
+
+    HttpAuthenticationMechanism getWrapped() {
+        return httpAuthenticationMechanism;
+    }
+
+    @Override
+    public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response,
+            HttpMessageContext httpMessageContext) throws AuthenticationException {
+        return getWrapped().validateRequest(request, response, httpMessageContext);
+    }
+
+    @Override
+    public AuthenticationStatus secureResponse(HttpServletRequest request, HttpServletResponse response,
+            HttpMessageContext httpMessageContext) throws AuthenticationException {
+        return getWrapped().secureResponse(request, response, httpMessageContext);
+    }
+
+    @Override
+    public void cleanSubject(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) {
+        getWrapped().cleanSubject(request, response, httpMessageContext);
+    }
+
+}
